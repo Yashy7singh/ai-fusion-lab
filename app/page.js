7b7d8dd0e85285053c064209f706a1d3054277e8
Yash Singh <ykvk2004@gmail.com>
2025-10-23 14:09:00 +0530

    5.Database Setup

--- app/page.js
+++ app/page.js
@@ -1,12 +1,9 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { useTheme } from "next-themes";
 import Image from "next/image";
 import ChatInputBox from "./_components/ChatInputBox";
 
 export default function Home() {
-  const {setTheme} = useTheme();
   return (
     <div>
       <ChatInputBox/>
