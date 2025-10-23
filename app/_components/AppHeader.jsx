38af7745cac746553b0197f4e69f885f6f545c9d
Yash Singh <ykvk2004@gmail.com>
2025-10-23 20:32:37 +0530

    clerk authentication added , updated footer

--- app/_components/AppHeader.jsx
+++ app/_components/AppHeader.jsx
@@ -7,7 +7,6 @@ function AppHeader() {
     <div className='p-3 w-full shadow flex justify-between items-center'>
         
         <SidebarTrigger />
-        <Button>Sign In</Button>
     </div>
   )
 }
